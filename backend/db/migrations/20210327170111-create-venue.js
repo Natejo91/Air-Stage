0cc3278b1a9e73e6f4ab2fe11884d222cb26c098
nathaniel <nathanieljortega@gmail.com>
2021-03-27 12:30:42 -0500

    added venue model and migration

---
+++ backend/db/migrations/20210327170111-create-venue.js
@@ -0,0 +1,56 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Venues', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING(100),
+        allowNull: false
+      },
+      desctiption: {
+        type: Sequelize.TEXT,
+        allowNull: false
+      },
+      maxGuests : {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      dailyCost: {
+        type: Sequelize.DECIMAL(6, 2),
+        allowNull: false
+      },
+      address: {
+        type: Sequelize.STRING(255),
+        allowNull: false
+      },
+      city: {
+        type: Sequelize.STRING(100),
+        allowNull: false
+      },
+      state: {
+        type: Sequelize.STRING(50),
+        allowNull: false
+      },
+      bookingImgUrl: {
+        type: Sequelize.STRING(100),
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Venues');
+  }
+};
---
+++ backend/db/models/venue.js
@@ -0,0 +1,41 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Venue = sequelize.define('Venue', {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    desctiption: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    maxGuests: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    dailyCost: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    state: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    bookingImgUrl: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, {});
+  Venue.associate = function(models) {
+    // associations can be defined here
+  };
+  return Venue;
+};
