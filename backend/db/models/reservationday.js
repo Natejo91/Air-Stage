282bb85b1b6d20e3ad3525395f4e46704325216a
nathaniel <nathanieljortega@gmail.com>
2021-03-27 14:53:58 -0500

    fixed syntax in reservationDays

--- backend/db/models/reservationday.js
+++ backend/db/models/reservationday.js
@@ -16,7 +16,6 @@ module.exports = (sequelize, DataTypes) => {
   }, {});
   ReservationDay.associate = function(models) {
     // associations can be defined here
-    }
   };
   return ReservationDay;
 };
